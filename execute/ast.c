9126a3e3320c0b42cdeca8a1bc3bd38fa868b7ca
Gael Mariani <lemarian@student.42.fr>
2024-12-03 14:01:21 +0100

    branche gael

---
+++ execute/ast.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemarian <lemarian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 13:32:45 by lemarian          #+#    #+#             */
+/*   Updated: 2024/11/27 16:35:07 by lemarian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+
+
+/*void	exec(t_ast_node head)
+{
+	if (head == word) ->should be a command
+	{
+		if (head->right == word) -> should be a file
+		{
+			check_file;
+			redirect input;
+		}
+		if (word == built-in)
+			exec (built-in)
+		else
+		{
+			search_path(word)
+			if (not found)
+				print("Command not found")
+			else
+				exec(command)
+		}
+	}
+	if (head == pipe)
+	{
+		create pipe;
+		fork;
+		redirect_output;
+		while (node != word)
+			traverse->left;
+		if (word->right == word)
+		{
+			check_file;
+			redirect_input;
+		}
+		if (built_in)
+			exec;
+		else
+			search and exec;
+	}
+}
+*/
+
