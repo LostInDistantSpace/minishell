d130998639f2b95199dfc0e32f6d0f43f6465939
Bilal Mouhib <bmouhib@made-f0Cr9s6.clusters.42paris.fr>
2024-12-05 15:21:51 +0100

    Improved prompt (removed most double_joins), removed name

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,25 +6,50 @@
 /*   By: bmouhib <bmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 23:00:40 by bmouhib           #+#    #+#             */
-/*   Updated: 2024/12/04 23:00:54 by bmouhib          ###   ########.fr       */
+/*   Updated: 2024/12/05 15:19:25 by bmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*get_dir(void)
+{
+	int		home_len;
+	char	*home;
+	char	*cwd;
+	char	*gwd;
+
+	home = getenv("HOME");
+	home_len = ft_strlen(home);
+	cwd = getcwd(NULL, 0);
+	if (!ft_strncmp(cwd, home, home_len))
+	{
+		gwd = malloc(ft_strlen(cwd) - home_len + 2 * sizeof(char));
+		gwd = ft_strjoin(BLUE BOLD "~", cwd + home_len);
+		free(cwd);
+		return (gwd);
+	}
+	return (ft_strjoin(BLUE BOLD, cwd));
+}
+
 char	*prompt(void)
 {
-	char	*dir;
 	char	*tmp;
+	char	*dir;
+	char	*user;
 	char	*prompt;
 
+	if (getenv("USER"))
+		user = ft_stroin(GREEN BOLD, getenv("USER"));
+	else
+		user = NULL;
 	dir = get_dir();
-	prompt = ft_double_join(GREEN, BOLD, getenv("USER"));
-	tmp = ft_double_join(prompt, "@", getenv("NAME"));
-	free(prompt);
-	prompt = ft_double_join(tmp, " ", BLUE);
-	free(tmp);
-	tmp = ft_double_join(prompt, dir, "$ ");
-	prompt = ft_strjoin(tmp, RESET_COLOR);
-	return (free(tmp), free(dir), prompt);
+	if (user)
+	{
+		tmp = ft_double_join(user, " ", dir);
+		prompt = ft_strjoin(tmp, RESET_COLOR "$ ");
+	}
+	else
+		prompt = ft_strjoin(dir, RESET_COLOR "$ ");
+	return (free(tmp), free(dir), free(user), prompt);
 }
