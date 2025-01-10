2833a12ba99f2986c44db005b10199aab982123b
Gael Mariani <lemarian@made-f0Cr8s2.clusters.42paris.fr>
2025-01-10 12:28:52 +0100

    fixed redirections

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: lemarian <lemarian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:22:40 by lemarian          #+#    #+#             */
-/*   Updated: 2025/01/09 18:15:53 by lemarian         ###   ########.fr       */
+/*   Updated: 2025/01/10 12:20:01 by lemarian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ void	exec(t_ast **head, t_env **start);
 void	create_pipe(t_ast *node, t_data *data);
 
 void	ft_redirect(t_ast *node, t_data *data);
-void	change_input(t_ast *node);
-void	change_output(t_ast *node);
+int		change_input(t_ast *node);
+int		change_output(t_ast *node);
 
 char	**get_env(t_env **env);
 
--- srcs/exec/handle_in_out.c
+++ srcs/exec/handle_in_out.c
@@ -6,7 +6,7 @@
 /*   By: lemarian <lemarian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 14:29:59 by lemarian          #+#    #+#             */
-/*   Updated: 2025/01/09 18:23:05 by lemarian         ###   ########.fr       */
+/*   Updated: 2025/01/10 12:21:37 by lemarian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,29 @@ void	restore_in_out(t_data *data)
 	close(data->save_out);
 }
 
-void	change_input(t_ast *node)
+int	change_input(t_ast *node)
 {
 	int	fd_in;
 
 	fd_in = open(node->args[0], O_RDONLY);
 	if (fd_in < 0)
-		return (perror(strerror(errno)));
+	{	
+		perror(node->args[0]);
+		return (0);
+	}
 	if (node->type == REDIR_HEREDOC)
 		unlink(node->args[0]);
 	if (dup2(fd_in, STDIN_FILENO) == -1)
 	{
 		close(fd_in);
-		return (perror(strerror(errno)));
+		(perror(NULL));
+		return (0);
 	}
 	close(fd_in);
+	return (1);
 }
 
-void	change_output(t_ast *node)
+int	change_output(t_ast *node)
 {
 	int	fd_out;
 
@@ -46,13 +51,18 @@ void	change_output(t_ast *node)
 	else
 		fd_out = open(node->args[0], O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd_out < 0)
-		return (perror(strerror(errno)));
+	{	
+		perror(node->args[0]);
+		return (0);
+	}
 	if (dup2(fd_out, STDOUT_FILENO) == -1)
 	{
 		close(fd_out);
-		return (perror(strerror(errno)));
+		perror(NULL);
+		return (0);
 	}
 	close(fd_out);
+	return (1);
 }
 
 void	ft_redirect(t_ast *node, t_data *data)
@@ -63,9 +73,15 @@ void	ft_redirect(t_ast *node, t_data *data)
 	while (node)
 	{
 		if (node->type == REDIR_IN || node->type == REDIR_HEREDOC)
-			change_input(node);
+		{	
+			if (!change_input(node))
+				return;
+		}
 		else if (node->type == REDIR_OUT || node->type == REDIR_APPEND)
-			change_output(node);
+		{	
+			if (!change_output(node))
+				return;
+		}
 		node = node->right;
 	}
 	if (!command)
