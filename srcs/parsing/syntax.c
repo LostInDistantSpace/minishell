7083d25bde2181a85a08b611a551e33afc8b826f
bmouhib <bmouhib@student.42.fr>
2025-01-20 17:32:00 +0100

    Added 0 and $ to expension

--- srcs/parsing/syntax.c
+++ srcs/parsing/syntax.c
@@ -6,7 +6,7 @@
 /*   By: bmouhib <bmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:35:28 by bmouhib           #+#    #+#             */
-/*   Updated: 2025/01/20 15:52:00 by bmouhib          ###   ########.fr       */
+/*   Updated: 2025/01/20 17:02:52 by bmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int	syntax_checker(char *str)
 	char	quote;
 
 	if (syntax_init(str, &quote, &i))
-		return (1);
+		return (1); //unexpected token
 	if (only_whitespace(str))
 		return (3);
 	quote = 0;
@@ -77,7 +77,7 @@ int	syntax_checker(char *str)
 			if (is_forbiddenchar(str[i]))
 				return (char_not_handled(str[i]));
 			if (is_spechar(str[i]) && check_special_char(str, i))
-				return (1);
+				return (1); //unexpected token near
 		}
 		i++;
 	}
